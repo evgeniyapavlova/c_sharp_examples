6c75e1738d6ab6095f158e2a634c72818c67a95a
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2023-10-14 12:20:15 +0300

    Add example 005 with condition

---
+++ 005_condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Input user name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Yes, it's Masha!");
+}
+else
+{
+    Console.Write("Hello, ");    
+    Console.WriteLine(username);    
+}
