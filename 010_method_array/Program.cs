adfd5dec6b8c510c891e451a8f8abfb7369413ed
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2023-10-15 20:28:22 +0300

    Add task with work through array with while and break operators

---
+++ 010_method_array/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+int n = arr.Length;
+int find = 6;
+int index = 0;
+
+while (index < n)
+{
+    if (arr[index] == find) break;
+    index++;
+}
+
+Console.WriteLine(index);
