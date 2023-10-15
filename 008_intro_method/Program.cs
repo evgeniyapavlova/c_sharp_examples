ab65cd297506494bf876017affef62d1226274cb
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2023-10-15 20:14:23 +0300

    Add task with function

---
+++ 008_intro_method/Program.cs
@@ -0,0 +1,16 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result; 
+}
+
+int a = 123, b=3378, c=5, d=9144 , e=3, f=47, g=6, h=22, i=8;
+
+int max1 = Max(a, b, c);
+int max2 = Max(d, e, f);
+int max3 = Max(g, h, i);
+int max = Max(max1, max2, max3);
+
+Console.WriteLine(max);
